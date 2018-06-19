40f6ec91ae02d24f1dc821c7546fef7a3512f43c
Flavio Pereira <flaviosantino@gmail.com>
2018-06-19 17:26:20 -0300

    Fixed key password variable

--- oracle-linux-instance/oracle-linux.tf
+++ oracle-linux-instance/oracle-linux.tf
@@ -6,6 +6,7 @@ variable "tenancy_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
+variable "private_key_password" {}
 variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 variable "region" {}
